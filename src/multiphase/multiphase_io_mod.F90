5d894e662078fc91e0ee83bc1f4a52815f1d5b86
Valentin Ruhs <valentin.ruhs@gmx.de>
2026-06-06 13:21:43 +0200

    wip(vds): multiphase test cases in mglet

--- src/multiphase/multiphase_io_mod.F90
+++ src/multiphase/multiphase_io_mod.F90
@@ -24,6 +24,9 @@ MODULE multiphase_io_mod
     IMPLICIT NONE
     PRIVATE 
 
+    REAL(realk), PARAMETER :: pi = 4.0_realk * atan(1.0_realk)
+    REAL(realk), PROTECTED :: initErr
+
     PUBLIC :: init_multiphase_io, finish_multiphase_io, update_velocity
 
 CONTAINS
@@ -46,6 +49,7 @@ CONTAINS
         REAL(realk), POINTER, CONTIGUOUS :: ddx(:), ddy(:), ddz(:)
         REAL(realk) :: minx, maxx, miny, maxy, minz, maxz
         REAL(realk) :: centx, centy, centz, rad, x, y, z, inside
+        REAL(realk) :: trueVol, apprVol
 
         CALL get_field(vff_f, "VFF")
         CALL get_field(dx_f, "DX"); CALL get_field(dy_f, "DY"); CALL get_field(dz_f, "DZ")
@@ -62,67 +66,154 @@ CONTAINS
             CALL ddx_f%get_ptr(ddx, igrid); CALL ddy_f%get_ptr(ddy, igrid); CALL ddz_f%get_ptr(ddz, igrid)
 
             SELECT CASE( test_multiphase )
-            CASE ( 'SphTra' )
-
-                centx = 0.2_realk ; centy = 0.2_realk ; rad = 0.1_realk
-                iSub = 128 ; jSub = 128 ; kSub = 128
-                nxDom = 16 ; nyDom = 16 ; nzDom = 16
-
-                ! Outer loop over cells
-                DO i = 1, ii ; DO j = 1, jj ; DO k = 1, kk
-
-                    inside = 0
-
-                    ! Inner loop over (.)Sub for refinement
-                    DO di = 1, iSub ; DO dj = 1, jSub ; DO dk = 1, kSub
-
-                        x = ( i + (di + 0.5_realk)/iSub ) / nxDom
-                        y = ( j + (dj + 0.5_realk)/jSub ) / nyDom
-                        z = ( k + (dk + 0.5_realk)/kSub ) / nzDom
-
-                        IF ( (x - centx)**2_realk + (y - centy)**2_realk + (z - centz)**2_realk <= rad**2_realk ) THEN
-                            inside = inside + 1
-                        ENDIF
-                        
-                    ENDDO ; ENDDO ; ENDDO
-
-                    vff(k,j,i) = inside / (iSub * jSub * kSub)
-
-                ENDDO ; ENDDO ; ENDDO
-
-            CASE ( 'VorBoF' )
-                continue
-            CASE ( 'VorBoC' )
-                                
-                centx = 0.5_realk ; centy = 0.75_realk ; rad = 0.15_realk
-                iSub = 256 ; jSub = 256
-                nxDom = 32 ; nyDom = 32
-                
+            CASE ( 'SphTrF' ) ! Sphere Translation Fine
+                !----------------------------------------------------
+                centx = 0.5_realk ; centy = 0.5_realk ; centz = 0.5_realk ; rad = 0.06875_realk
+                iSub = 32 ; jSub = 32 ; kSub = 32
+                nxDom = 160 ; nyDom = 160 ; nzDom = 160
+                WRITE(*,'(A23)') "Start VFF Init...      "
                 ! Outer loop over cells
                 DO i = 3, ii-2 ; DO j = 3, jj-2 ; DO k = 3, kk-2
-
                     inside = 0.0_realk
-
                     ! Inner loop over (.)Sub for refinement
-                    DO di = 0, iSub-1 ; DO dj = 0, jSub-1
-
+                    DO di = 0, iSub-1 ; DO dj = 0, jSub-1 ; DO dk = 0, kSub-1
                         x = minx + ( i-3 + (di + 0.5_realk)/iSub ) / nxDom
                         y = miny + ( j-3 + (dj + 0.5_realk)/jSub ) / nyDom
-
-                        IF ( (x - centx)**2.0_realk + (y - centy)**2.0_realk <= rad**2.0_realk ) THEN
+                        z = minz + ( k-3 + (dk + 0.5_realk)/kSub ) / nzDom
+                        IF ( (x - centx)**2.0_realk + &
+                             (y - centy)**2.0_realk + &
+                             (z - centz)**2.0_realk <= rad**2.0_realk ) THEN
                             inside = inside + 1.0_realk
                         ENDIF
-                        
-                    ENDDO ; ENDDO
-                    
-                    vff(k,j,i) = inside / (iSub * jSub)
-
+                    ENDDO ; ENDDO ; ENDDO
+                    vff(k,j,i) = inside / (iSub * jSub * kSub)
                 ENDDO ; ENDDO ; ENDDO
-
-            CASE ( 'CylAdF' )
-                continue
-            CASE ( 'CylAdC' )
-                continue
+                trueVol = 4.0_realk / 3.0_realk * pi * rad**3.0_realk
+                apprVol = SUM(vff) * ( dx(1) * dy(1) * dz(1) )
+                CALL print_statistics(iSub, jSub, kSub, trueVol, apprVol)
+                !----------------------------------------------------
+            CASE ( 'SphTrC' ) ! Sphere Translation Coarse
+                !----------------------------------------------------
+                centx = 0.5_realk ; centy = 0.5_realk ; centz = 0.5_realk ; rad = 0.06875_realk
+                iSub = 32 ; jSub = 32 ; kSub = 32
+                nxDom = 80 ; nyDom = 80 ; nzDom = 80
+                WRITE(*,'(A23)') "Start VFF Init...      "
+                ! Outer loop over cells
+                DO i = 3, ii-2 ; DO j = 3, jj-2 ; DO k = 3, kk-2
+                    inside = 0.0_realk
+                    ! Inner loop over (.)Sub for refinement
+                    DO di = 0, iSub-1 ; DO dj = 0, jSub-1 ; DO dk = 0, kSub-1
+                        x = minx + ( i-3 + (di + 0.5_realk)/iSub ) / nxDom
+                        y = miny + ( j-3 + (dj + 0.5_realk)/jSub ) / nyDom
+                        z = minz + ( k-3 + (dk + 0.5_realk)/kSub ) / nzDom
+                        IF ( (x - centx)**2.0_realk + &
+                             (y - centy)**2.0_realk + &
+                             (z - centz)**2.0_realk <= rad**2.0_realk ) THEN
+                            inside = inside + 1.0_realk
+                        ENDIF
+                    ENDDO ; ENDDO ; ENDDO
+                    vff(k,j,i) = inside / (iSub * jSub * kSub)
+                ENDDO ; ENDDO ; ENDDO
+                trueVol = 4.0_realk / 3.0_realk * pi * rad**3.0_realk
+                apprVol = SUM(vff) * ( dx(1) * dy(1) * dz(1) )
+                CALL print_statistics(iSub, jSub, kSub, trueVol, apprVol)
+                !----------------------------------------------------
+            CASE ( 'VorBoF' ) ! Vortex-in-a-Box Fine
+                !----------------------------------------------------
+                centx = 0.5_realk ; centy = 0.75_realk ; centz = 0.0_realk ; rad = 0.15_realk
+                iSub = 512 ; jSub = 512 ; kSub = 1
+                nxDom = 128 ; nyDom = 128 ; nzDom = 5
+                WRITE(*,'(A23)') "Start VFF Init...      "
+                ! Outer loop over cells
+                DO i = 3, ii-2 ; DO j = 3, jj-2 ; DO k = 3, kk-2
+                    inside = 0.0_realk
+                    ! Inner loop over (.)Sub for refinement
+                    DO di = 0, iSub-1 ; DO dj = 0, jSub-1
+                        x = minx + ( i-3 + (di + 0.5_realk)/iSub ) / nxDom
+                        y = miny + ( j-3 + (dj + 0.5_realk)/jSub ) / nyDom
+                        IF ( (x - centx)**2.0_realk + &
+                             (y - centy)**2.0_realk <= rad**2.0_realk ) THEN
+                            inside = inside + 1.0_realk
+                        ENDIF
+                    ENDDO ; ENDDO
+                    vff(k,j,i) = inside / (iSub * jSub * kSub)
+                ENDDO ; ENDDO ; ENDDO
+                trueVol = pi * rad**2.0_realk * ( maxy - miny )
+                apprVol = SUM(vff) * ( dx(1) * dy(1) * dz(1) )
+                CALL print_statistics(iSub, jSub, kSub, trueVol, apprVol)
+                !----------------------------------------------------
+            CASE ( 'VorBoC' ) ! Vortex-in-a-Box Coarse
+                !----------------------------------------------------
+                centx = 0.5_realk ; centy = 0.75_realk ; centz = 0.0_realk ; rad = 0.15_realk
+                iSub = 512 ; jSub = 512 ; kSub = 1
+                nxDom = 32 ; nyDom = 32 ; nzDom = 5
+                WRITE(*,'(A23)') "Start VFF Init...      "
+                ! Outer loop over cells
+                DO i = 3, ii-2 ; DO j = 3, jj-2 ; DO k = 3, kk-2
+                    inside = 0.0_realk
+                    ! Inner loop over (.)Sub for refinement
+                    DO di = 0, iSub-1 ; DO dj = 0, jSub-1
+                        x = minx + ( i-3 + (di + 0.5_realk)/iSub ) / nxDom
+                        y = miny + ( j-3 + (dj + 0.5_realk)/jSub ) / nyDom
+                        IF ( (x - centx)**2.0_realk + &
+                             (y - centy)**2.0_realk <= rad**2.0_realk ) THEN
+                            inside = inside + 1.0_realk
+                        ENDIF
+                    ENDDO ; ENDDO
+                    vff(k,j,i) = inside / (iSub * jSub * kSub)
+                ENDDO ; ENDDO ; ENDDO
+                trueVol = pi * rad**2.0_realk * ( maxy - miny )
+                apprVol = SUM(vff) * ( dx(1) * dy(1) * dz(1) )
+                CALL print_statistics(iSub, jSub, kSub, trueVol, apprVol)
+                !----------------------------------------------------
+            CASE ( 'CylAdF' ) ! Cylinder Advection Fine
+                !----------------------------------------------------
+                centx = 0.2_realk ; centy = 0.2_realk ; centz = 0.0_realk ; rad = 0.1_realk
+                iSub = 512 ; jSub = 512 ; kSub = 1
+                nxDom = 80 ; nyDom = 80 ; nzDom = 5
+                WRITE(*,'(A23)') "Start VFF Init...      "
+                ! Outer loop over cells
+                DO i = 3, ii-2 ; DO j = 3, jj-2 ; DO k = 3, kk-2
+                    inside = 0.0_realk
+                    ! Inner loop over (.)Sub for refinement
+                    DO di = 0, iSub-1 ; DO dj = 0, jSub-1
+                        x = minx + ( i-3 + (di + 0.5_realk)/iSub ) / nxDom
+                        y = miny + ( j-3 + (dj + 0.5_realk)/jSub ) / nyDom
+                        IF ( (x - centx)**2.0_realk + &
+                             (y - centy)**2.0_realk <= rad**2.0_realk ) THEN
+                            inside = inside + 1.0_realk
+                        ENDIF
+                    ENDDO ; ENDDO
+                    vff(k,j,i) = inside / (iSub * jSub * kSub)
+                ENDDO ; ENDDO ; ENDDO
+                trueVol = pi * rad**2.0_realk * ( maxy - miny )
+                apprVol = SUM(vff) * ( dx(1) * dy(1) * dz(1) )
+                CALL print_statistics(iSub, jSub, kSub, trueVol, apprVol)
+                !----------------------------------------------------
+            CASE ( 'CylAdC' ) ! Cylinder Advection Coarse
+                !----------------------------------------------------
+                centx = 0.2_realk ; centy = 0.2_realk ; centz = 0.0_realk ; rad = 0.1_realk
+                iSub = 512 ; jSub = 512 ; kSub = 1
+                nxDom = 16 ; nyDom = 16 ; nzDom = 5
+                WRITE(*,'(A23)') "Start VFF Init...      "
+                ! Outer loop over cells
+                DO i = 3, ii-2 ; DO j = 3, jj-2 ; DO k = 3, kk-2
+                    inside = 0.0_realk
+                    ! Inner loop over (.)Sub for refinement
+                    DO di = 0, iSub-1 ; DO dj = 0, jSub-1
+                        x = minx + ( i-3 + (di + 0.5_realk)/iSub ) / nxDom
+                        y = miny + ( j-3 + (dj + 0.5_realk)/jSub ) / nyDom
+                        IF ( (x - centx)**2.0_realk + &
+                             (y - centy)**2.0_realk <= rad**2.0_realk ) THEN
+                            inside = inside + 1.0_realk
+                        ENDIF
+                    ENDDO ; ENDDO
+                    vff(k,j,i) = inside / (iSub * jSub * kSub)
+                ENDDO ; ENDDO ; ENDDO
+                trueVol = pi * rad**2.0_realk * ( maxy - miny )
+                apprVol = SUM(vff) * ( dx(1) * dy(1) * dz(1) )
+                CALL print_statistics(iSub, jSub, kSub, trueVol, apprVol)
+                !----------------------------------------------------
             END SELECT
         ENDDO
 
@@ -140,6 +231,33 @@ CONTAINS
 
         continue
     END SUBROUTINE finish_multiphase_io
+
+    !================================================================
+
+    SUBROUTINE print_statistics(iSub, jSub, kSub, trueVol, apprVol)
+    !----------------------------------------------------------------
+    !   What it does:
+    !   
+    !----------------------------------------------------------------
+
+        ! Subroutine arguments
+        INTEGER(intk), INTENT(in) :: iSub, jSub, kSub
+        REAL(realk), INTENT(in) :: trueVol, apprVol
+
+        ! Local variables
+        ! None
+
+        initErr = ABS( trueVol - apprVol )
+
+        WRITE(*,'(A23)')          "-----------------------"
+        WRITE(*,'(A23)')          "Multi-Phase VFF Init.  "
+        WRITE(*,'(A10,1X,I12)')   "iSub:     " , iSub
+        WRITE(*,'(A10,1X,I12)')   "jSub:     " , jSub
+        WRITE(*,'(A10,1X,I12)')   "kSub:     " , kSub
+        WRITE(*,'(A10,1X,E12.6)') "initErr:  " , initErr
+        WRITE(*,'(A23)') ""
+
+    END SUBROUTINE print_statistics
 
     !================================================================
 
@@ -165,7 +283,6 @@ CONTAINS
         REAL(realk), POINTER, CONTIGUOUS :: ddx(:), ddy(:), ddz(:)
         INTEGER(intk) :: kk, jj, ii, k, j, i
         REAL(realk) :: magnitude, fac(6)
-        REAL(realk), PARAMETER :: pi = 4.0_realk * atan(1.0_realk)
         REAL(realk), ALLOCATABLE :: psi(:,:,:)
 
         CALL get_field(dx_f, "DX")
@@ -194,7 +311,7 @@ CONTAINS
             CALL ddy_f%get_ptr(ddy, igrid)
             CALL ddz_f%get_ptr(ddz, igrid)
 
-            IF ( test_multiphase == 'SphTra' ) THEN
+            IF ( test_multiphase == 'SphTrF' .OR. test_multiphase == 'SphTrC' ) THEN
                 fac = [0.7686000, 0.5968000, 0.1035700, &
                        0.5514000, 0.2242010, 0.2512981]
                 magnitude = 0.0125_realk
@@ -240,7 +357,7 @@ CONTAINS
                             DO k = 2, kk-1
                                 IF ( vff(k,j,i) >= 0.0_realk ) THEN
                                     u(k,j,i) = 0.016_realk
-                                    v(k,j,i) = 0.0_realk
+                                    v(k,j,i) = 0.016_realk
                                     w(k,j,i) = 0.0_realk
                                 END IF
                             END DO
